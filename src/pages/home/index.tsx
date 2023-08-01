181cc98c6cf2a5d004a0f40d652bc03dbd0db836
Julia Sanchez <juliasanchez@pop-os.localdomain>
2023-08-01 12:25:44 -0300

    feat: first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from '@/components/Sidebar';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <Sidebar />
+    </>
+  );
+}
+
+export default Home;
