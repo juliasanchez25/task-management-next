cb16027fe3e7ea1826b3886e8bbf2aa1970a0bd1
Julia Sanchez <juliasanchez@pop-os.localdomain>
2023-10-05 21:47:55 -0300

    chore: remove babel and add to next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    styledComponents: {
+      'ssr': true,
+      'displayName': true,
+      'preprocess': false
+    }
+  }
+};
 
 module.exports = nextConfig;
